5a79c2b6b8d07da89c8320f1f690421e842f1f72
Ariel_Naiman <a0527633600@gmail.com>
2025-11-03 16:11:13 +0200

    adding FieldAgent and CyberAgent class

--- practice/OOP/Agent Operation Exercises.py
+++ practice/OOP/Agent Operation Exercises.py
@@ -2,15 +2,49 @@ class Agent:
     def __init__(self, code_name: str, clearance_level: int):
         self.code_name = code_name
         self._clearance_level = clearance_level
-    
+
     def report(self):
-        print("Agent {} reporting. Clearance Level: {}".format(self.code_name, self._clearance_level))
-        
+        return """Agent {} reporting. Clearance Level: {}""".format(self.code_name, self._clearance_level)
+
+    def get_clearance_level(self):
+        print(self._clearance_level)
+
+    def set_clearance_level(self, level: int):
+        if level < 1 or level > 10:
+            return "You cannot change the level below 1 or above 10."
+        self._clearance_level = level
+
 class Mission():
     def __init__(self, mission_name: str, target_location: str, assigned_agent: Agent):
         self.mission_name = mission_name
         self.target_location = target_location
         self.assigned_agent = assigned_agent
-        
+
     def brief(self):
         print("Mission: {}, Target: {}, Agent: {}".format(self.mission_name, self.target_location, self.assigned_agent.code_name))
+
+class FieldAgent(Agent):
+    def __init__(self, code_name: str, clearance_level: int, region: str):
+        super().__init__(code_name, clearance_level)
+        self.region = region
+
+    def report(self):
+        return super().report() + self.region
+
+class CyberAgent(Agent):
+    def __init__(self, code_name, clearance_level, specialty):
+        super().__init__(code_name, clearance_level)
+        self.specialty = specialty
+
+    def report(self):
+        return super().report() + self.specialty
+    
+def print_agent(list_agent: list):
+    for i in list_agent:
+        print(i.report())
+        
+l = []
+l.append(Agent("ariel", 2))
+l.append(FieldAgent("pinchas", 2, "ashdid"))
+l.append(CyberAgent("eviatar", 2, "nerves"))
+print_agent(l)
