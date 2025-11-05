50b4bf56c31f07de24b15bf1360ba499e9119798
Ariel_Naiman <a0527633600@gmail.com>
2025-11-05 12:36:18 +0200

    adding StrikeOption class

--- practice/military_simulation/army_inventory.py
+++ practice/military_simulation/army_inventory.py
@@ -1,3 +1,4 @@
+# Exercise 1
 class Weapon:
     total_weapons = 0
     def __init__(self, name: str, ammo: int):
@@ -12,13 +13,38 @@ class Soldier:
         self.weapon = weapon
     
     def report(self):
-        print(f"name: {self.name}, rank: {self.rank}, weapon: {self.weapon}.")
+        print(f"name: {self.name}, rank: {self.rank}, weapon: {self.weapon.name}, {self.weapon.ammo}.")
 
 class Unit:
-    def __init__(self, unit_name: str, commander: Soldier, soldiers: list[Soldier]):
+    def __init__(self, unit_name: str, commander: Soldier, soldiers: list[Soldier], strike_option):
         self.unit_name = unit_name
         self.commander = commander
         self.soldiers = soldiers
+        self.strike_option = strike_option
     
     def briefing(self):
         print(f"unit name: {self.unit_name}, commander: {self.commander}.")
+
+
+# Exercise 2
+class StrikeOption:
+    def __init__(self, name: str, ammo: int):
+        self.name = name
+        self.ammo = ammo
+    
+    def strike(self):
+        pass
+
+class Tank(StrikeOption):
+    def __init__(self, name, ammo):
+        super().__init__(name, ammo)
+    
+    def strike(self):
+        print("Tank")
+
+class Drone(StrikeOption):
+    def __init__(self, name, ammo):
+        super().__init__(name, ammo)
+
+    def strike(self):
+        print("Drone")
